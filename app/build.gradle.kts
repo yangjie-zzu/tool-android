7e1a0973ec023ba61e05740971e9caf440cdc1d4
yangjie-zzu <1342986150@qq.com>
2026-09-18 04:35:36 +0800

    版本升级至1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.yukino.tool"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
